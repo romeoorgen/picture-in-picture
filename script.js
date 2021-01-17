7b0bf6c676ae9d5bf00030dfe79a33f496a6678d
romeo <romeoorgen2@gmail.com>
2021-01-17 12:49:27 +0100

    correction script.js

--- script.js
+++ script.js
@@ -3,24 +3,24 @@ const button = document.getElementById('button');
 
 // Prompt to select media stream, pass to video element, then play
 async function selectMediaStream() {
-    try {
-        const mediaStream = await navigator.mediaDevices.getDisplayMedia();
-        videoElement.srcObject = mediaStream;
-        videoElement.onloadedmetadata = () => {
-            videoElement.play();
-        }
-    }  catch (error) {
-// Catch Error here
-
-    }
+  try {
+    const mediaStream = await navigator.mediaDevices.getDisplayMedia();
+    videoElement.srcObject = mediaStream;
+    videoElement.onloadedmetadata = () => {
+      videoElement.play();
+    };
+  } catch (error) {
+    // Catch Error Here
+  }
 }
+
 button.addEventListener('click', async () => {
-    // Disable button
-    button.disabled = true;
-    // Start Picture in picture
-    await videoElement.requestPictureInPicture();
-    // Reset Button
-    button.disabled = false;
+  // Disable Button
+  button.disabled = true;
+  // Start Picture in Picture
+  await videoElement.requestPictureInPicture();
+  // Reset Button
+  button.disabled = false;
 });
 
 // On Load
